e0e4a0e1f38c35f262c59247c8a73975853fdb99
devlouix <devlouix@gmail.com>
2023-01-18 00:10:01 +0100

    first_commit_after_publishing_for_review_at_appstores

---
+++ app/src/main/java/com/devlouix/tiltawake/BlankScreen.java
@@ -0,0 +1,14 @@
+package com.devlouix.tiltawake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BlankScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank_screen);
+    }
+}
---
+++ app/src/main/java/com/devlouix/tiltawake/PrivacyPolicy.java
@@ -0,0 +1,23 @@
+package com.devlouix.tiltawake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+
+        WebView mWebView = (WebView) findViewById(R.id.activity_privacy_policy_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://tiltawake.blogspot.com/p/privacy-policy.html");
+    }
+}
